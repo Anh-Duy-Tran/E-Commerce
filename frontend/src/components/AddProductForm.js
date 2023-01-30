3d05353c1fe29fae33b2d2c49adb5cc1083f1dda
Anh Duy Tran <anh.2.tran@tuni.fi>
2023-01-31 00:22:39 +0200

    prepare to deploy new version with new functionalities for admin user

--- frontend/src/components/AddProductForm.js
+++ frontend/src/components/AddProductForm.js
@@ -1,10 +1,12 @@
-import { Box, Container, Typography, TextField, Grid, useTheme } from "@mui/material"
+import { Box, Container, Typography, TextField, Grid, useTheme, Button } from "@mui/material"
 import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Select from '@mui/material/Select';
 import Chip from '@mui/material/Chip';
 import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
+import CircleIcon from '@mui/icons-material/Circle';
 import { useState } from "react";
 
 const ITEM_HEIGHT = 48;
@@ -20,6 +22,7 @@ const MenuProps = {
 
 
 const names = [
+  'new',
   'all',
   'men',
   'women',
@@ -29,13 +32,13 @@ const names = [
   'accessories'
 ];
 
-const letter_size = ["XS", "S", "M", "L", "XL", "XXL"]
-const number_size = ['30', '32', '34', '36', '38', '40', '42', '44', '46', '48', '50', '52', '54', '56', '58', '60'];
+const Alpha = ["XS", "S", "M", "L", "XL", "XXL"]
+const Numeric = ['30', '32', '34', '36', '38', '40', '42', '44', '46', '48', '50', '52', '54', '56', '58', '60'];
 
-function getStyles(name, personName, theme) {
+function getStyles(name, productType, theme) {
   return {
     fontWeight:
-      personName.indexOf(name) === -1
+      productType.indexOf(name) === -1
         ? theme.typography.fontWeightRegular
         : theme.typography.fontWeightMedium,
   };
@@ -44,17 +47,41 @@ function getStyles(name, personName, theme) {
 
 const AddProductForm = () => {
   const theme = useTheme();
-  const [personName, setPersonName] = useState([]);
+  const [ productType, setProductType ] = useState([]);
+  const [ productSize, setProductSize ] = useState([]);
+  
+  const [ sizeType, setSizeType ] =  useState('')
+  const [ sizeOption, setSizeOption ] = useState(["Please choose a size type"])
 
-  const handleChange = (event) => {
+  const [ numColor, setNumColor ] = useState(1);
+  const [ previewColor, setPreviewColor ] = useState(['']);
+
+  const handleProductTypeChange = (event) => {
     const {
       target: { value },
     } = event;
-    setPersonName(
+    setProductType(
       // On autofill we get a stringified value.
       typeof value === 'string' ? value.split(',') : value,
     );
   };
+
+  const handleProductSizeChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setProductSize(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  }
+
+  const handleSizeType = (event) => {
+    setSizeType(event.target.value);
+
+    setProductSize([]);
+    setSizeOption([... event.target.value === 'Numeric size' ? Numeric : Alpha]);
+  }
 
   return (
     <Container component="main" sx={{mt : 15}}>
@@ -62,7 +89,7 @@ const AddProductForm = () => {
             ADD NEW PRODUCT
       </Typography>
 
-      <Typography sx={{fontFamily: 'Futura', mt: 2 }} component="h1">
+      <Typography sx={{fontFamily: 'Futura', mt: 4 }} component="h1">
             Product information:
       </Typography>
 
@@ -72,75 +99,224 @@ const AddProductForm = () => {
             flexDirection: 'column'
           }}
       >
-        <Container component="item">
-          <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="product-name"
-                  label="Product name"
-                  name="product-name"
-                  autoComplete="product-name"
-          />
-        </Container>
-        <Container component="item">
-          <TextField
-                  margin="none"
-                  required
-                  fullWidth
-                  id="product-description"
-                  label="Product description"
-                  name="product-description"
-                  autoComplete="product-description"
-                  multiline
-          />
-
+        <Container sx={{mt : 1}}>
+          <FormControl variant="outlined" fullWidth>
+            <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="product-name"
+                    label="Product name"
+                    name="product-name"
+                    autoComplete="product-name"
+            />
+          </FormControl>
         </Container>
 
-        <Container component="item" sx={{display : 'flex'}}>
+        <Container sx={{mt : 1}} >
+          <FormControl variant="outlined" fullWidth>
+            <TextField
+                    margin="none"
+                    required
+                    fullWidth
+                    id="product-description"
+                    label="Product description"
+                    name="product-description"
+                    autoComplete="product-description"
+                    multiline
+            />
 
-          <OutlinedInput
-            sx={{mt:1}}
-            id="outlined-adornment-weight"
-            endAdornment={<InputAdornment position="end">€</InputAdornment>}
-            aria-describedby="outlined-weight-helper-text"
-            placeholder="Price"
-          />
+          </FormControl>
+        </Container>
 
-          <InputLabel id="multiple-chip-label">Chip</InputLabel>
-          <Select
-            sx={{ml: 1, mt: 1, flexGrow: 1}}
-            labelId="multiple-chip-label"
-            id="multiple-chip"
-            multiple
-            value={personName}
-            onChange={handleChange}
-            input={<OutlinedInput id="select-multiple-chip" label="abc" placeholder="Product types" />}
-            renderValue={(selected) => (
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                {selected.map((value) => (
-                  <Chip key={value} label={value} />
-                ))}
-              </Box>
-            )}
-            MenuProps={MenuProps}
-          >
-          {names.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
+        <Container  sx={{display : 'flex', mt: 2, gap : 1.5}}>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="product-price">Price</InputLabel>
+            <OutlinedInput
+              required
+              id="product-price"
+              label='Price'
+              endAdornment={<InputAdornment position="end">€</InputAdornment>}
+              aria-describedby="product price"
+            />
+          </FormControl>
+
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="product-type-label">Product type</InputLabel>
+            <Select
+              sx={{flexGrow: 1}}
+              labelId="product-type-label"
+              id="product-type"
+              multiple
+              value={productType}
+              onChange={handleProductTypeChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Product type"/>}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
             >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
+              {names.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, productType, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Container>
 
-        <Typography sx={{fontFamily: 'Futura', mt: 2 }} component="h1">
+        <Container sx={{display : 'flex', mt : 2, gap : 1.5}}>
+          <FormControl variant="outlined" sx={{ width : '200px'}} >
+            <InputLabel htmlFor="size-type">Size type</InputLabel>
+            <Select 
+              id="size-type"
+              placeholder="abc"
+              value={sizeType}
+              label="Size type"
+              onChange={handleSizeType}
+            >
+              <MenuItem value="Numeric size">
+                Numeric size
+              </MenuItem>
+              <MenuItem value="Alpha size">
+                Alpha size
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl variant="outlined" fullWidth>
+            <InputLabel id="product-size-label">Product size</InputLabel>
+            <Select
+              sx={{flexGrow: 1}}
+              labelId="product-size-label"
+              id="product-size"
+              multiple
+              value={productSize}
+              onChange={handleProductSizeChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Product size"/>}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {sizeOption.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, productType, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+        </Container>
+
+        <Typography sx={{fontFamily: 'Futura', mt: 6 }} component="h1">
             Product colors and images:
         </Typography>
 
+        <Container sx={{display : "flex", justifyContent: "center"}}>
+          <Button onClick={() => {
+            setPreviewColor((crr) => {
+              let temp = [...crr]
+              temp.push('')
+              return temp
+            })
+            setNumColor(numColor + 1)}
+          }>
+            Add new color
+          </Button>
+        </Container>
+
+        <Box>
+          {
+            Array.from(Array(numColor).keys()).map(
+              i => (
+                  <Container key={i} sx={{ display : "flex"}}>
+                    <FormControl>
+                      <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id={`color-${i}-name`}
+                                label={`Color${i} name`}
+                                name={`color-${i}-name`}
+                                autoComplete="color-name"
+                      />
+                      <TextField
+                                margin="dense"
+                                required
+                                fullWidth
+                                id={`color-${i}-code`}
+                                label={`Color${i} code (Hex)`}
+                                name={`color-${i}-code`}
+                                onChange={(e) => {
+                                  const temp = [...previewColor]
+                                  temp[i] = `#${e.target.value}`
+                                  setPreviewColor(temp)
+                                }}
+                                autoComplete='color-code'
+                      />
+                      <CircleIcon sx={{color : previewColor[i] }}></CircleIcon>  
+                    </FormControl>
+
+                    <FormControl sx={{flexGrow : 1}}>
+                      <TextField
+                                  sx={{ml : 1.5}}
+                                  margin="normal"
+                                  required
+                                  fullWidth
+                                  id="color-product-image"
+                                  label="Product image links (seperate by  ' ; ' )"
+                                  name="color-product-image"
+                                  autoComplete="image-links"
+                                  multiline
+                      />
+                      {
+                        i + 1 == numColor && numColor !== 1 
+                        ? <Button 
+                                  onClick={() => 
+                                  {
+                                    setNumColor(numColor - 1)
+                                    setPreviewColor((crr) => {
+                                      let temp = [...crr]
+                                      temp.pop()
+                                      return temp
+                                    })
+                                  }}
+                                  sx={{mt : 1, ml : 2}}> Remove color
+                          </Button>
+                        : null
+                      }
+                      
+                    </FormControl>
+                  </Container>
+              )
+            )
+          }
+
+        </Box>
+        
+      <Button 
+            sx={{mt : 4, ml : 2, mr : 2}}
+            type="submit"
+      >
+        See preview
+      </Button>
       </Box>
     </Container>
   )
